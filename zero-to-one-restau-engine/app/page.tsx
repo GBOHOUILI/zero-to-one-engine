2de2ef0816058abb09b289b4035ffefc9ed4b2a8
GBOHOUILI <eldomoreogbohouili@gmail.com>
2026-01-27 05:25:46 +0100

    Initial commit avec l'application Next.js

---
+++ zero-to-one-restau-engine/app/page.tsx
@@ -0,0 +1,7 @@
+export default function Page() {
+  return (
+    <h1 className="text-3xl font-bold underline text-green-500">
+      Initialisation Du Projet de site pour les restaurants !
+    </h1>
+  );
+}
